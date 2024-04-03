5450bfd36e22547c7068a9da15e813fa9b3c9c5d
Lidia Haile <lidiahaile@gmail.com>
2024-04-03 15:57:25 +0100

    HTML elements are accessed using querySelector

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "./style.scss";
+
 //Elements access
 const resultBox = document.querySelector<HTMLDivElement>(".calculator__result-box");
 const operators = document.querySelector<HTMLButtonElement>("#operators");
@@ -11,7 +12,6 @@ const equalTo =document.querySelector<HTMLButtonElement>(".calculator__button2--
 if(!resultBox || !operators || !valueNum || !equalTo){
     throw new Error ("Issue with selectors");
 }
-
 
 
 
