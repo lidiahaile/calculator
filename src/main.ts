e467fc93bbee84e2eded42fffde3ac0bc95f33c2
Lidia Haile <lidiahaile@gmail.com>
2024-04-03 15:43:10 +0100

    layout and styling are added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import "./style.scss";
+//Elements access
+const resultBox = document.querySelector<HTMLDivElement>(".calculator__result-box");
+const operators = document.querySelector<HTMLButtonElement>("#operators");
+const valueNum = document.querySelectorAll<HTMLButtonElement>("#number");
+const equalTo =document.querySelector<HTMLButtonElement>(".calculator__button2--equal");
+
+
+
+//Throw an error
+if(!resultBox || !operators || !valueNum || !equalTo){
+    throw new Error ("Issue with selectors");
+}
+
+
 
 
 
