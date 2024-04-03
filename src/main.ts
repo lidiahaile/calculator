d964ea18df0a0d48842fbc544f964152e70d4c88
Lidia Haile <lidiahaile@gmail.com>
2024-04-03 17:54:39 +0100

    numbers and oprator variables are added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import "./style.scss";
 
-//Elements access
+//Elements HTML access
 const resultBox = document.querySelector<HTMLDivElement>(".calculator__result-box");
 const operators = document.querySelector<HTMLButtonElement>("#operators");
 const valueNum = document.querySelectorAll<HTMLButtonElement>("#number");
 const equalTo =document.querySelector<HTMLButtonElement>(".calculator__button2--equal");
+const clearBtn =document.querySelector<HTMLButtonElement>(".calculator__button1--c");
+
 
 
 
@@ -13,6 +15,9 @@ if(!resultBox || !operators || !valueNum || !equalTo){
     throw new Error ("Issue with selectors");
 }
 
+let variableOne = "";
+let variableTwo = "";
+let Operator = "";
 
 
 
